cbf1f9e93af66b5a82df5d87346c922debed0375
huyong <you@example.com>
2024-09-06 16:13:32 +0800

    项目初始构建

---
+++ gulimall-prouct/src/main/java/com/atguigu/gulimall/gulimallprouct/GulimallProuctApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.gulimallprouct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProuctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProuctApplication.class, args);
+    }
+
+}
---
+++ gulimall-prouct/src/test/java/com/atguigu/gulimall/gulimallprouct/GulimallProuctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.gulimallprouct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProuctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
