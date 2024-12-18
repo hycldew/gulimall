c60e912fde83371798ded8105465d8aa113f794a
huyong <you@example.com>
2024-12-18 17:58:11 +0800

    树形

--- gulimall-prouct/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ gulimall-prouct/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.atguigu.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,13 +32,13 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 树形结构
      */
-    @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+    @RequestMapping("/list/tree")
+    public R list(){
+        List<CategoryEntity> categoryEntityList = categoryService.listWithTree();
 
-        return R.ok().put("page", page);
+        return R.ok().put("data", categoryEntityList);
     }
 
 
--- gulimall-prouct/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-prouct/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.atguigu.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -25,5 +31,33 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
         return new PageUtils(page);
     }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1.查出所有分类
+        List<CategoryEntity> categoryEntityList = baseMapper.selectList(null);
+
+        //2.组装成父子的树形结构
+        //2.1.找到所有的一级分类
+        List<CategoryEntity> collectParent = categoryEntityList.stream().filter(e -> e.getParentCid() == 0)
+                .peek(e -> e.setChildren(getChildrens(e,categoryEntityList)))
+                .sorted(Comparator.comparingInt(CategoryEntity::getSort))
+                .collect(Collectors.toList());
+
+
+        return collectParent;
+    }
+
+    // 递归查找
+    private List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> all){
+        List<CategoryEntity> collect = all.stream()
+                .filter(e -> Objects.equals(e.getParentCid(), root.getCatId()))
+                .peek(e -> e.setChildren(getChildrens(e, all)))
+                .sorted(Comparator.comparingInt(CategoryEntity::getSort))
+                .collect(Collectors.toList());
+
+        return null;
+    }
+
 
 }
